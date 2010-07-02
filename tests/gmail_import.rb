2e4e5c4d5e44567a35b4c5977c43caaa338c3caf
vovka <sch.icq@gmail.com>
2010-07-02 13:36:43 +0300

    tabs replaced with double spaces

--- tests/gmail_import.rb
+++ tests/gmail_import.rb
@@ -4,16 +4,12 @@ require File.join(File.dirname(__FILE__), 'helpers.rb')
 require 'yaml'
 
 
-file = File.new(File.join(File.dirname(__FILE__), '../config/user.info'), "r")
-yaml_user_info = ''
-while (line = file.gets)
-	yaml_user_info << line
-end
-file.close
 
-user_info = YAML::load yaml_user_info
+
+user_info = YAML::load(
+  load_config(File.join(File.dirname(__FILE__), '../config/user.info')) )
 
 contacts = ImportContacts::Gmail.get user_info
 #contacts.class.should == Array
-save_to_file 'gmail_contacts.info', contacts
+save_to_file 'gmail_contacts.info', contacts.body
 
--- tests/helpers.rb
+++ tests/helpers.rb
@@ -1,7 +1,16 @@
 def save_to_file filename, contacts
-	File.open('gmail_contacts.out', 'w') do |f|
-		f.puts contacts
-	end
+  File.open('gmail_contacts.out', 'w') do |f|
+    f.puts contacts
+  end
 end
 
-p "OK"
+def load_config config_file_path
+  file = File.new(config_file_path, "r")
+  yaml_user_info = ''
+  while (line = file.gets)
+    yaml_user_info << line
+  end
+  file.close
+  yaml_user_info
+end
+
